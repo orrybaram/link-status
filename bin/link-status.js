58900dd1f2af5eb9307fb79354d34fa1d3c7e734
Dan Phillips <danmakenoise@gmail.com>
2017-01-09 02:39:12 -0500

    command follows symlinks to destination in nvm

--- bin/link-status.js
+++ bin/link-status.js
@@ -10,9 +10,41 @@ function _handleShellResponse (error, response) {
   var opts = {
     error: error,
     console: console,
+    getAndReplaceSource: _getAndReplaceSource,
     shouldDisplaySource: shouldDisplaySource
   };
 
   handleShellResponse(response, opts);
+}
+
+function _getAndReplaceSource (input, callback) {
+  var sourceMatcher = /-> \.\.\/(\.\.\/.+)/;
+  var privateSourceMatcher = /-> \.\.\/\.\.\/(\.\.\/.+)/;
+
+  var sourceMatch = input.match(sourceMatcher);
+  var privateSourceMatch = input.match(privateSourceMatcher);
+
+  var source = sourceMatch && sourceMatch.length && sourceMatch[1];
+  var privateSource = privateSourceMatch && privateSourceMatch.length && privateSourceMatch[1];
+
+  if (!source && !privateSource) {
+    return callback(input);
+  }
+
+  shellExec('readlink ' + source, function (error, response) {
+    if (error || !response) {
+      shellExec('readlink ' + privateSource, function (errorTwo, responseTwo) {
+        if (errorTwo || !responseTwo) {
+          return callback(input);
+        }
+
+        return callback(input.replace('../../' + privateSource, responseTwo.replace('\n', '')));
+      });
+
+      return;
+    }
+
+    return callback(input.replace('../' + source, response.replace('\n', '')));
+  });
 }
 
